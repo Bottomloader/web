61db7d727f99398356d0f6d86b9feb0eda047741
Ivaylo <imsamlolll@gmail.com>
2025-04-12 22:57:45 +0300

    Update mainButtons.js

--- src/web/mainButtons.js
+++ src/web/mainButtons.js
@@ -4,7 +4,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const privacyButton = document.getElementById("fpp");
 
   aboutButton.addEventListener("click", function () {
-    window.location.href = "/about";
+    window.history.pushState({}, "", "/about");
+    fetch("about.html")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        return response.text();
+      })
+      .then(html => {
+        document.body.innerHTML = html;
+      })
+      .catch(error => {
+        console.error("Error loading the page! Error: ", error);
+        document.body.innerHTML = `
+          <div style="text-align: center; padding: 20px;">
+            <h1 style="color: red;">Couldn't load page</h1>
+            <p style="font-size: 1.2rem;">There was an issue loading the requested page. Please try again later.</p>
+          </div>
+        `;
+      });
   });
 
   tosButton.addEventListener("click", function () {
