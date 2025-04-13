389a2e8efd6ca4a72e386cc4c091e2a4c01a955f
Bottomloader <imsamlolll@gmail.com>
2025-04-14 00:39:20 +0300

    Login Test

--- src/web/mainButtons.js
+++ src/web/mainButtons.js
@@ -4,26 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const privacyButton = document.getElementById("fpp");
 
   aboutButton.addEventListener("click", function () {
-    window.history.pushState({}, "", "/about");
-    fetch("about.html")
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok.");
-        }
-        return response.text();
-      })
-      .then(html => {
-        document.body.innerHTML = html;
-      })
-      .catch(error => {
-        console.error("Error loading the page! Error: ", error);
-        document.body.innerHTML = `
-          <div style="text-align: center; padding: 20px;">
-            <h1 style="color: red;">Couldn't load page</h1>
-            <p style="font-size: 1.2rem;">There was an issue loading the requested page. Please try again later.</p>
-          </div>
-        `;
-      });
+    window.location.href = "/about";
   });
 
   tosButton.addEventListener("click", function () {
